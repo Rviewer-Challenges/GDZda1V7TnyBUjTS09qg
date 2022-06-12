268f332ae0e6709ca3e513d62b16c9cd95046685
German Solano <kontrolg1239@gmail.com>
2022-06-11 23:26:48 -0400

    feat(<App />): :sparkles: Add React Native Elements

---
+++ constants/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@rneui/themed";
+
+const theme = createTheme({
+  darkColors: {
+    background: "#15202B",
+    black: "#f7f9f9",
+    grey2: "#8B98A5",
+    primary: "#1DA1F2"
+  }
+});
+
+export default theme;
