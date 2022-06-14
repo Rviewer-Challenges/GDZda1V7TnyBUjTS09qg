1012030e0b47fe33df20e101742e31d2d36bc905
German Solano <kontrolg1239@gmail.com>
2022-06-14 08:12:31 -0400

    refactor(<EnrichedText />): :label: Extend props to allow customize the container <Text />

--- components/common/widgets/EnrichedText.tsx
+++ components/common/widgets/EnrichedText.tsx
@@ -1,4 +1,4 @@
-import { Text, useTheme } from "@rneui/themed";
+import { Text, TextProps, useTheme } from "@rneui/themed";
 import Linkify from "linkify-react";
 import "linkify-plugin-mention";
 import "linkify-plugin-hashtag";
@@ -9,21 +9,24 @@ const externalLinkTypes = ["url", "email"];
 const linkTextTagName = (_operator: string, type: string) =>
   externalLinkTypes.includes(type) ? Anchor : Text;
 
-interface EnrichedTextProps {
+interface EnrichedTextProps extends TextProps {
   children: string;
   options?: any;
 }
 
-function EnrichedText({ children, options }: EnrichedTextProps) {
+function EnrichedText({ children, options, ...textProps }: EnrichedTextProps) {
   const { theme } = useTheme();
 
   return (
     <Linkify
+      {...textProps}
       tagName={Text}
       options={{
         ...options,
         tagName: linkTextTagName,
-        attributes: { style: { color: theme.colors.primary } }
+        attributes: {
+          style: { color: theme.colors.primary, ...options?.style }
+        }
       }}
     >
       {children}
