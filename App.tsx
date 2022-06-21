72d61c8271f24a5963362937cb9f663a0d528157
German Solano <kontrolg1239@gmail.com>
2022-06-21 06:58:18 -0400

    feat: :sparkles: Add custom <Text /> with custom font family

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@rneui/themed";
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import theme from "./constants/theme";
@@ -10,8 +11,11 @@ import Navigation from "./navigation";
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [fontsLoaded] = useFonts({
+    "GT America Regular": require("./assets/fonts/GT America Regular.ttf")
+  });
 
-  if (!isLoadingComplete) {
+  if (!isLoadingComplete || !fontsLoaded) {
     return null;
   } else {
     return (
